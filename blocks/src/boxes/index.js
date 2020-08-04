ce28a97cc42be88319f7a4e025f5b804be0d84cd
Kenny Poncio <kenpoc4@gmail.com>
2020-08-03 23:10:18 -0600

    Primer bloque terminado

--- blocks/src/boxes/index.js
+++ blocks/src/boxes/index.js
@@ -1,5 +1,17 @@
 import { registerBlockType } from '@wordpress/blocks';
-import { RichText } from '@wordpress/editor';
+import { RichText, InspectorControls, ColorPalette, BlockControls, AlignmentToolbar } from '@wordpress/editor';
+import { PanelBody } from '@wordpress/components';
+
+/**
+*           7 Pasos para crear un bloque Gutenberg
+        1.- Importar el componente(s) que utilizaremos
+        2.- Colocar el componente donde se desea utilizar
+        3.- Crear una funcion que lea los contenidos
+        4.- Registrar un atributo
+        5.- Extraer el contenido desde los Props
+        6.- Guardar el contenido con setAttributes
+        7.- Leer los contenidos gusrdados en save()
+*/
 
 registerBlockType(
     'pg/basic',
@@ -8,20 +20,131 @@ registerBlockType(
         description:    'Este es el primer bloque',
         icon:           'smiley',
         category:       'elpizza',
-        edit: () => {
+        attributes: {
+            headingBox : {
+                type: 'string',
+                source: 'html',
+                selector: '.box h2'
+            },
+            textoBox: {
+                type: 'string',
+                source: 'html',
+                selector: '.box p'
+            },
+            colorFondo: {
+                type: 'string'
+            },
+            colorTexto: {
+                type: 'string'
+            },
+            alineacionContenido: {
+                type: 'string',
+                default: 'center'
+            }
+        },
+        edit: (props) => {
+
+            console.log(props);
+
+            // Extrayendo el contenido desde los Props
+            const { attributes: {headingBox, textoBox, colorFondo, colorTexto, alineacionContenido}, setAttributes } = props;
+
+            const onChangeHeadingBox = nuevoHeading => {
+                setAttributes({headingBox : nuevoHeading});
+            }
+
+            const onChangeTextoBox = nuevoTexto => {
+                setAttributes({textoBox : nuevoTexto });
+            }
+
+            const onChangeColorFondo = nuevoColor => {
+                setAttributes({colorFondo : nuevoColor});
+            }
+
+            const onChangeColorTexto = nuevoColor => {
+                setAttributes({colorTexto : nuevoColor})
+            }
+
+            const onChangeAlinearContenido = nuevaAlineacion => {
+                setAttributes({alineacionContenido : nuevaAlineacion})
+            }
+
             return(
-                <div>
-                    <h2>
-                        <RichText
-                            placeholder="Agrega el encabezado"
+                <>
+                    <InspectorControls>
+                        <PanelBody
+                            title={'Color de fondo'}
+                            initialOpen={true}
+                        >
+                            <div className="components-base-control">
+                                <div className="components-base-control__field">
+                                    <label className="components-base-control__label">
+                                        Color de Fondo
+                                    </label>
+                                    <ColorPalette 
+                                        onChange={onChangeColorFondo}
+                                        value={colorFondo}
+                                    />
+                                </div>
+                            </div>
+                        </PanelBody>
+                        <PanelBody
+                            title={'Color de Texto'}
+                            initialOpen={false}
+                        >
+                            <div className="components-base-control">
+                                <div className="components-base-control__field">
+                                    <label className="components-base-control__label">
+                                        Color de Texto
+                                    </label>
+                                    <ColorPalette 
+                                        onChange={onChangeColorTexto}
+                                        value={colorTexto}
+                                    />
+                                </div>
+                            </div>
+                        </PanelBody>
+                    </InspectorControls>
+
+                    <BlockControls>
+                        <AlignmentToolbar
+                            onChange={onChangeAlinearContenido}
                         />
-                    </h2>
-                </div>
+                    </BlockControls>
+                    <div className="box" style={{backgroundColor : colorFondo, textAlign : alineacionContenido}}>
+                        <h2 style={{color: colorTexto}}>
+                            <RichText
+                                placeholder = "Agrega el encabezado"
+                                onChange = {onChangeHeadingBox}
+                                value={headingBox}
+                            />
+                        </h2>
+                        <p style={{color: colorTexto}}>
+                            <RichText
+                                placeholder="Agrega el texto"
+                                onChange={onChangeTextoBox}
+                                value={textoBox}
+                            />
+                        </p>
+                    </div>
+                </>
             )
         },
-        save: () => {
+        save: (props) => {
+            console.log(props);
+
+            // Extrayendo el contenido desde los Props
+            const { attributes: {headingBox, textoBox, colorFondo, colorTexto, alineacionContenido} } = props;
+
             return(
-                <h2>Esto es el save</h2>
+                <div className="box" style={{backgroundColor : colorFondo, textAlign : alineacionContenido}}>
+                    <h2 style={{color: colorTexto}}>
+                        <RichText.Content value={headingBox}/>
+                    </h2>
+                    <p style={{color: colorTexto}}>
+                        <RichText.Content value={textoBox}/>
+                    </p>
+                </div>
             )
         }
     }
